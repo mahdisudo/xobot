45db1bd906cdc01263690eb502d4f77d740a78d9
mahdisudo <mehdisudo@gmail.com>
2016-09-01 02:38:32 +0430

    Update XO_Game.php

--- XO_Game.php
+++ XO_Game.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-define('BOT_TOKEN', 'Your Token');
+define('BOT_TOKEN', '229137572:AAEbK2VnQbGjfv0vZXkWBYEnTWnIPZnl5zc');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
